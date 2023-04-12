9a9b93289b7c51ff4dacd4bc21dc414e8bb5d648
amd <gajeambi@amd.com>
2023-04-12 12:57:51 +0000

    serializers

--- dn/inbox/forms.py
+++ dn/inbox/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from users.models import Team
 from inbox.models import Messages
-from django.forms import ModelForm
+from django.forms import ModelForm, Form
 
 
 class BroadcastForm(ModelForm):
---
+++ dn/inbox/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class YourSerializer(serializers.Serializer):
+   """"""
+   sender = serializers
+   receivers = serializers
